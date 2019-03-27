9f2af99e815109c2e367ce09eaf4c7e87608f8f4
Juan Diego Molina Bareño <juan.diego@kometsales.com>
2019-03-26 20:11:06 -0500

    Avances segunda entrega

--- src/helpers.js
+++ src/helpers.js
@@ -7,16 +7,16 @@ let listaCursos = [];
 const listarCursos = () => {
     try {
         listaCursos = require('./listado-cursos.json');
-    } catch(err) {
+    } catch (err) {
         listaCursos = [];
     }
-    
+
 };
 
 const crearCurso = (curso) => {
     listarCursos();
     let duplicado = listaCursos.find(elem => elem.id == curso.id);
-    if(!duplicado){
+    if (!duplicado) {
         listaCursos.push(curso);
         guardar();
         return mostrarCursos();
@@ -26,19 +26,19 @@ const crearCurso = (curso) => {
                      </div>';
         return texto;
     }
-    
+
 }
 
 const guardar = () => {
     let datos = JSON.stringify(listaCursos);
     fs.writeFile(directorioSrc + '/listado-cursos.json', datos, (error) => {
-        if(error) throw (error)
+        if (error) throw (error)
         console.log('Archivo creado con cursos');
     });
 };
 
 hbs.registerHelper('guardarCurso', (id, nombre, descripcion, valor, modalidad, intensidad) => {
-    if(id && nombre) {
+    if (id && nombre) {
         return crearCurso({
             id: id,
             nombre: nombre,
@@ -51,6 +51,43 @@ hbs.registerHelper('guardarCurso', (id, nombre, descripcion, valor, modalidad, i
     } else {
         return mostrarCursos();
     }
+});
+
+hbs.registerHelper('verCursoInteresado', () => {
+    listarCursos();
+    let texto = '<div class="accordion" id="accordionExample">';
+    let index = 0;
+    let collapse = true;
+    let classShow = '';
+    listaCursos = listaCursos.filter(curso => curso.estado != 'Cerrado');
+    listaCursos.forEach(curso => {
+        collapse = index != 0 ? false : true;
+        classShow = index != 0 ? '' : 'show';
+        texto = texto +
+            '<div class="card"> \
+            <div class="card-header" id="heading' + index + '"> \
+            <h2 class="mb-0"> \
+                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse' + index + '" aria-expanded="' + collapse + '" aria-controls="collapse' + index + '">' +
+            'Nombre del curso ' + curso.nombre + '</br>' +
+            'Valor ' + curso.valor +
+            '</button> \
+            </h2> \
+            </div> \
+            <div id="collapse' + index + '" class="collapse ' + classShow + '" aria-labelledby="heading' + index + '" data-parent="#accordionExample"> \
+            <div class="card-body"> \
+                <ul class="list-group"> \
+                    <li class="list-group-item"> Nombre: ' + curso.nombre + '</li>' +
+            '<li class="list-group-item"> Descripcion: ' + curso.descripcion + '</li>' +
+            '<li class="list-group-item"> Modalidad: ' + curso.modalidad + '</li>' +
+            '<li class="list-group-item"> Intensidad: ' + curso.intensidad + '</li>' +
+            '</ul> \
+            </div> \
+            </div> \
+        </div>';
+        index++;
+    });
+    texto = texto + '</div>'
+    return texto;
 });
 
 const mostrarCursos = () => {
@@ -68,7 +105,7 @@ const mostrarCursos = () => {
                  </tr> \
                  </thead> \
                  <tbody>';
-        listaCursos.forEach(curso => {
+    listaCursos.forEach(curso => {
         texto = texto +
             '<tr>' +
             '<th scope="row">' + curso.id + '</th>' +
